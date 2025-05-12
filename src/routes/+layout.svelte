3a1ccb89e7ebed5b2c48c4c9df0c908cd0809605
Jake Nagle <jake.nagle54@gmail.com>
2025-05-12 20:00:25 +0100

    Dynamic loading of pages added through content and adding of +layout.svelte page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="tabs notification">
+		<ul>
+			<li><a href="/">Dashboard</a></li>
+			<li><a href="/login">Login</a></li>
+			<li><a href="/signup">Signup</a></li>
+		</ul>
+	</div>
+	<slot />
+</div>
