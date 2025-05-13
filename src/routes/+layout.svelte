7bc96a4631bb4c69636305d63305848e3dd635ba
Jake Nagle <jake.nagle54@gmail.com>
2025-05-13 19:48:42 +0100

    Modified the 'routes/+page.svelte' page to add an icon to the login button, show my Placemark text and icon and other content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,3 @@
 <div class="container">
-	<div class="tabs notification">
-		<ul>
-			<li><a href="/">Dashboard</a></li>
-			<li><a href="/login">Login</a></li>
-			<li><a href="/signup">Signup</a></li>
-		</ul>
-	</div>
 	<slot />
 </div>
