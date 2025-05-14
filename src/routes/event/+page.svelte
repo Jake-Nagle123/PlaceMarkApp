bd4200b7588e84349e29fb1b40b30b6ffae3bdaa
Jake Nagle <jake.nagle54@gmail.com>
2025-05-14 20:05:44 +0100

    Event Form added to page

--- src/routes/event/+page.svelte
+++ src/routes/event/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { subTitle } from "$lib/runes.svelte";
+  import EventForm from "./EventForm.svelte";
+
   subTitle.text = "Events";
 </script>
 
-<p>Add your event here!</p>
+<EventForm />
