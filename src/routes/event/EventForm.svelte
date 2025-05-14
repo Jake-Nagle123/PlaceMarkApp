c17bbbc28302b3a36a544b104b975606368cda01
Jake Nagle <jake.nagle54@gmail.com>
2025-05-14 20:17:10 +0100

    Basic Event Form added to page

--- src/routes/event/EventForm.svelte
+++ src/routes/event/EventForm.svelte
@@ -1,4 +1,31 @@
 <script lang="ts">
 </script>
 
-<div></div>
+<div>
+  <div class="field">
+    <label class="label" for="amount">Enter Amount:</label>
+    <input class="input" id="amount" name="amount" type="number" />
+  </div>
+  <div class="field">
+    <div class="control">
+      <label class="label" for="amount">Select Payment Method:</label>
+      <input class="radio" type="radio" /> paypal
+      <input class="radio" type="radio" /> direct
+    </div>
+  </div>
+  <div class="field">
+    <label class="label" for="amount">Select Stadium:</label>
+    <div class="select">
+      <select>
+        <option>Walsh Park</option>
+        <option>Croke Park</option>
+        <option>Páirc Uí Chaoimh</option>
+      </select>
+    </div>
+  </div>
+  <div class="field">
+    <div class="control">
+      <button class="button is-success is-fullwidth">Submit</button>
+    </div>
+  </div>
+</div>
