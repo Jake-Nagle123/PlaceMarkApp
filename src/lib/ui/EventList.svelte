34b70b44a059e5a905b7ea65eb20fb0a948cbc2e
Jake Nagle <jake.nagle54@gmail.com>
2025-05-16 11:51:47 +0100

    Event list updated with test data

--- src/lib/ui/EventList.svelte
+++ src/lib/ui/EventList.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
   export let events = [
     {
-      amount: ,
-      city: "",
-      sport: "",
-      rating: "",
-      stadium: { ground: "" },
-      latitude: "",
-      longitude: ""
+      amount: 40,
+      city: "Waterford",
+      sport: "Hurling",
+      rating: "8.9",
+      stadium: { ground: "Walsh Park" },
+      latitude: "53.36075",
+      longitude: "-6.251333"
     },
-  ]
+    {
+      amount: 120,
+      city: "Dublin",
+      sport: "Soccer",
+      rating: "6.2",
+      stadium: { ground: "Aviva Stadium" },
+      latitude: "53.3352",
+      longitude: "6.2284"
+    },
+    {
+      amount: 65,
+      city: "Limerick",
+      sport: "Rugby",
+      rating: "7.2",
+      stadium: { ground: "Thomond Park" },
+      latitude: "52.6742",
+      longitude: "8.6425"
+    },
+  ];
 </script>
+
